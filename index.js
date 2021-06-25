e332aa371fc0089b4ca24776fffe65dc296c309d
zeverotti <zeverotti@gmail.com>
2021-06-25 10:39:08 +0200

    fix: typo

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.command('map', async (ctx) => {
     const stream = await geocoding(query, zoom);
     ctx.replyWithPhoto({ source: stream });
   } catch (error) {
-    ctx.reply('An error occured: '+error.message);
+    ctx.reply('An error occurred: '+error.message);
   }
 })
 
