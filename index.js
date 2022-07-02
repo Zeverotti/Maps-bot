52c25dd243d2e86e4b8066138d6cb8cb464ac65b
Zeverotti <zeverotti@gmail.com>
2022-07-02 14:17:00 +0200

    feat: add zoom function

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
+const { Markup } = require('telegraf');
 require('dotenv').config();
 const axios = require('axios');
 const { Telegraf } = require('telegraf');
 
-async function geocoding(cityname, zoom = 8) {
+async function geocoding(coordinates, zoom = 8) {
   return new Promise(async(resolve, reject) => {
       try {
-        const response = await axios.get(`https://jz-software.herokuapp.com/api/application/weather?location=${cityname}`);
-        const coordinates = response.data[0].coordinates;
         const fileStream = await axios({ 
           method: 'get', 
           url: `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${coordinates[1]},${coordinates[0]},${zoom},0.00,0.00/1280x720@2x?access_token=${process.env.TOKENMAP}`, 
           responseType: 'stream',
         });
         resolve(fileStream.data);
+      } catch (error) {
+        reject(error);
+      }
+  })
+}
+
+async function getCoordinates(cityname) {
+  return new Promise(async(resolve, reject) => {
+      try {
+        const response = await axios.get(`https://jz-software.herokuapp.com/api/application/weather?location=${cityname}`);
+        const coordinates = response.data[0].coordinates;
+        resolve(coordinates);
       } catch (error) {
         reject(error);
       }
@@ -29,11 +40,39 @@ bot.command('map', async (ctx) => {
     return;
   }
   try {
-    const stream = await geocoding(query, zoom);
-    ctx.replyWithPhoto({ source: stream });
+    let coordinates = await getCoordinates(query);
+    const stream = await geocoding(coordinates, zoom);
+    coordinates = `${coordinates[0]};${coordinates[1]}`; 
+    ctx.replyWithPhoto({ source: stream },{ reply_markup: Markup.inlineKeyboard([
+    Markup.callbackButton('zoom +', `zoom-${coordinates}-${zoom}-up`),
+    Markup.callbackButton('zoom -', `zoom-${coordinates}-${zoom}-down`), 
+]),
+});
+    console.log(coordinates);
   } catch (error) {
     ctx.reply('An error occurred: '+error.message);
   }
 })
+
+bot.action(/^zoom/, async (ctx) => {
+  try{
+    const args = ctx.match.input.split('-');
+    let coordinates = args[1].split(';').map(e => parseFloat(e));
+    let zoom = parseInt(args[2]);
+    zoom = args[3] === "up" ? zoom + 1 : zoom - 1;
+    const stream = await geocoding(coordinates, zoom);
+    coordinates = coordinates.join(';');
+    await ctx.editMessageMedia({type: 'photo', media:{ source: stream}}, { reply_markup: Markup.inlineKeyboard([
+      Markup.callbackButton('zoom +', `zoom-${coordinates}-${zoom}-up`),
+      Markup.callbackButton('zoom -', `zoom-${coordinates}-${zoom}-down`), 
+    ])
+    });
+    await ctx.answerCbQuery();
+} catch (error) {
+  ctx.reply('An error occurred: '+ error.message);
+}
+    
+});
+
 
 bot.launch()
